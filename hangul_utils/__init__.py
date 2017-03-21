6ddc8fb94e4f789d14447a0b3d0cbed5dba5b806
Kang Min Yoo <kaniblurous@gmail.com>
2017-03-21 11:22:15 +0900

    fixed formatting

--- hangul_utils/__init__.py
+++ hangul_utils/__init__.py
@@ -1,52 +1,61 @@
-#encoding: UTF-8
+# encoding: UTF-8
+
+from __future__ import unicode_literals
 
 import sys
+import six
 import functools
 
-if sys.version_info < (3,):
+
+def __c(x):
+    return six.unichr(x)
+
+
+def __items(x):
+    return six.iteritems(x)
+
+
+if sys.version_info[0] <= 2:
     import codecs
+
 
     def __u(x):
         return codecs.unicode_escape_decode(x)[0]
-
-    def __c(x):
-        return unichr(x)
-
-    def __items(x):
-        return x.iteritems()
 else:
     def __u(x):
         return x
-
-    def __c(x):
-        return chr(x)
-
-    def __items(x):
-        return x.items()
 
 TYPE_INITIAL = 0x001
 TYPE_MEDIAL = 0x010
 TYPE_FINAL = 0x100
 
-INITIALS = list(map(__c, [0x3131, 0x3132, 0x3134, 0x3137, 0x3138, 0x3139, 0x3141, 0x3142, 0x3143, 0x3145,
-                          0x3146, 0x3147, 0x3148, 0x3149, 0x314a, 0x314b, 0x314c, 0x314d, 0x314e]))
+INITIALS = list(map(__c, [0x3131, 0x3132, 0x3134, 0x3137, 0x3138, 0x3139,
+                          0x3141, 0x3142, 0x3143, 0x3145, 0x3146, 0x3147,
+                          0x3148, 0x3149, 0x314a, 0x314b, 0x314c, 0x314d,
+                          0x314e]))
 
-MEDIALS = list(map(__c, [0x314f, 0x3150, 0x3151, 0x3152, 0x3153, 0x3154, 0x3155, 0x3156, 0x3157, 0x3158,
-                         0x3159, 0x315a, 0x315b, 0x315c, 0x315d, 0x315e, 0x315f, 0x3160, 0x3161, 0x3162,
-                         0x3163]))
+MEDIALS = list(map(__c, [0x314f, 0x3150, 0x3151, 0x3152, 0x3153, 0x3154,
+                         0x3155, 0x3156, 0x3157, 0x3158, 0x3159, 0x315a,
+                         0x315b, 0x315c, 0x315d, 0x315e, 0x315f, 0x3160,
+                         0x3161, 0x3162, 0x3163]))
 
-FINALS = list(map(__c, [0x3131, 0x3132, 0x3133, 0x3134, 0x3135, 0x3136, 0x3137, 0x3139, 0x313a,
-                        0x313b, 0x313c, 0x313d, 0x313e, 0x313f, 0x3140, 0x3141, 0x3142, 0x3144, 0x3145,
-                        0x3146, 0x3147, 0x3148, 0x314a, 0x314b, 0x314c, 0x314d, 0x314e]))
+FINALS = list(map(__c, [0x3131, 0x3132, 0x3133, 0x3134, 0x3135, 0x3136,
+                        0x3137, 0x3139, 0x313a, 0x313b, 0x313c, 0x313d,
+                        0x313e, 0x313f, 0x3140, 0x3141, 0x3142, 0x3144,
+                        0x3145, 0x3146, 0x3147, 0x3148, 0x314a, 0x314b,
+                        0x314c, 0x314d, 0x314e]))
 
 CHAR_LISTS = {TYPE_INITIAL: INITIALS, TYPE_MEDIAL: MEDIALS, TYPE_FINAL: FINALS}
 CHAR_SETS = dict(map(lambda x: (x[0], set(x[1])), __items(CHAR_LISTS)))
 CHARSET = functools.reduce(lambda x, y: x.union(y), CHAR_SETS.values(), set())
-CHAR_INDICES = dict(map(lambda x: (x[0], dict([(c, i) for i, c in enumerate(x[1])])), __items(CHAR_LISTS)))
+CHAR_INDICES = dict(
+    map(lambda x: (x[0], dict([(c, i) for i, c in enumerate(x[1])])),
+        __items(CHAR_LISTS)))
 
 
 def check_syllable(x):
     return 0xAC00 <= ord(x) <= 0xD7A3
+
 
 def jamo_type(x):
     t = 0x000
@@ -68,7 +77,8 @@ def split_syllable_char(x):
         raise ValueError("Input string must have exactly one character.")
 
     if not check_syllable(x):
-        raise ValueError("Input string does not contain a valid Korean character.")
+        raise ValueError(
+            "Input string does not contain a valid Korean character.")
 
     diff = ord(x) - 0xAC00
     _m = diff % 28
@@ -81,9 +91,12 @@ def split_syllable_char(x):
     if not final_index:
         result = (INITIALS[initial_index], MEDIALS[medial_index])
     else:
-        result = (INITIALS[initial_index], MEDIALS[medial_index], FINALS[final_index - 1])
+        result = (
+            INITIALS[initial_index], MEDIALS[medial_index],
+            FINALS[final_index - 1])
 
     return result
+
 
 def join_jamos_char(initial, medial, final=None):
     """
@@ -93,7 +106,8 @@ def join_jamos_char(initial, medial, final=None):
     :param final: final jamo.
     :return: A syllable.
     """
-    if initial not in CHAR_SETS[TYPE_INITIAL] or medial not in CHAR_SETS[TYPE_MEDIAL] or (final and final not in CHAR_SETS[TYPE_FINAL]):
+    if initial not in CHAR_SETS[TYPE_INITIAL] or medial not in CHAR_SETS[
+        TYPE_MEDIAL] or (final and final not in CHAR_SETS[TYPE_FINAL]):
         raise ValueError("Given Jamo characters are not valid.")
 
     initial_ind = CHAR_INDICES[TYPE_INITIAL][initial]
@@ -107,6 +121,7 @@ def join_jamos_char(initial, medial, final=None):
     assert check_syllable(result)
 
     return result
+
 
 def split_syllables(string):
     """
@@ -124,6 +139,7 @@ def split_syllables(string):
         new_string += new_c
 
     return new_string
+
 
 def join_jamos(string):
     """
@@ -148,7 +164,7 @@ def join_jamos(string):
             try:
                 result = join_jamos_char(*new_queue)
             except ValueError:
-                #Invalid jamo combination
+                # Invalid jamo combination
                 result = "".join(new_queue)
         else:
             result = None
@@ -188,32 +204,3 @@ def join_jamos(string):
         new_string += flush(queue)
 
     return new_string
-
-def __gen(n):
-    from cStringIO import StringIO
-    import random
-    s = StringIO()
-    for i in range(n):
-        s.write(random.choice(INITIALS + MEDIALS + FINALS).encode("utf8"))
-    return s.getvalue().decode("utf8")
-
-def main():
-    print(split_syllable_char(u"안"))
-    print(split_syllables(u"안녕하세요"))
-    sentence = u"앞 집 팥죽은 붉은 팥 풋팥죽이고, 뒷집 콩죽은 햇콩 단콩 콩죽.우리 집 깨죽은 검은 깨 깨죽인데 사람들은 햇콩 단콩 콩죽 깨죽 죽먹기를 싫어하더라."
-    s = split_syllables(sentence)
-    print(s)
-
-    sentence2 = join_jamos(s)
-    print(sentence2)
-
-    print(sentence == sentence2)
-
-    for i in range(10):
-        print(join_jamos(__gen(20)))
-
-    assert join_jamos_char(*split_syllable_char(u"안")) == u"안"
-
-
-if __name__ == "__main__":
-    main()
